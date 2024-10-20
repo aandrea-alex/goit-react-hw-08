eb51c13b25c2a6a7022cc085fe39f70760615edb
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-10-20 17:32:26 +0200

    Update NotFoundPage.jsx

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,6 +1,4 @@
-import SectionTitle from '../../components/SectionTitle/SectionTitle';
 import DocumentTitle from '../../components/DocumentTitle';
-import { ERR_404 } from '../../auxiliary/constants';
 import styles from './NotFoundPage.module.css';
 const NotFoundPage = () => {
   return (
@@ -8,7 +6,7 @@ const NotFoundPage = () => {
       {' '}
       <DocumentTitle>NotFoundPage</DocumentTitle>
       <div className={styles.container}>
-        <SectionTitle>{ERR_404}</SectionTitle>
+        <p>Page not found</p>
       </div>
     </>
   );
